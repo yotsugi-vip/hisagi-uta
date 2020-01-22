56c4ab618a40d12e631fa33d223c342747e98dd0
yotsugi <harukipdr@gmail.com>
2020-01-23 00:33:28 +0900

    add calc win

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header_img from './picture/utsHisagi.jpg';
-import { match_result, team_hisagi, uta_movie_url, hisagi_movie_url } from './data';
+import React, { Component } from "react";
+import "./App.css";
+import Header_img from "./picture/utsHisagi.jpg";
+import { match_result, uta_movie_url, hisagi_movie_url } from "./data";
 
-function Header(){
-  return(
+function hisagiWinCalc(order) {
+  var a = 0;
+  switch (order) {
+    case 1:
+      match_result.order1.forEach(match => (match.team1.win ? a++ : 0));
+      break;
+    case 2:
+      match_result.order2.forEach(match => (match.team1.win ? a++ : 0));
+      break;
+    default:
+      break;
+  }
+  return a;
+}
+
+function Header() {
+  return (
     <header className="App-header">
       <img src={Header_img} alt="header banner" />
     </header>
   );
 }
 
-function Body(){
-  return(
-    <body>
-      {match_result.order1.map((team, i)=>(
-        <div key={i}>
-          <p>{team.rule}</p>
-          <p>{team.team1.member.player}</p>
-          <p>{team.team2.member.player}</p>
-          <p><a href={uta_movie_url+"?t="+team.uta_movie}>試合リンク 炉龍うた</a></p>
-          <p><a href={hisagi_movie_url+"?t="+team.hisagi_movie}>試合リンク 楸</a></p>
-        </div>
-      ))}
-       {match_result.order2.map((team, i)=>(
-        <div key={i}>
-          <p>{team.rule}</p>
-          <p>{team.team1.member.player}</p>
-          <p>{team.team2.member.player}</p>
-          <p><a href={uta_movie_url+"?t="+team.uta_movie}>試合リンク 炉龍うた</a></p>
-          <p><a href={hisagi_movie_url+"?t="+team.hisagi_movie}>試合リンク 楸</a></p>
-        </div>
-      ))}
-    </body>
+function Body() {
+  return (
+    <div>
+      <details>
+        <summary>
+          第一周
+          <p>HISAGI WIN {hisagiWinCalc(1)}</p>
+          <p>UTA WIN {5-hisagiWinCalc(1)}</p>
+        </summary>
+        {match_result.order1.map((match, i) => (
+          <div key={i}>
+            <div className="ul-team">
+              <ul className="li-all">
+                {match.team1.member.player.map((player, i) => (
+                  <li key={i}>{player}</li>
+                ))}
+              </ul>
+              <ul className="li-all">
+                {match.team2.member.player.map((player, i) => (
+                  <li key={i}>{player}</li>
+                ))}
+              </ul>
+            </div>
+            {hisagi_movie_url}
+            {uta_movie_url}
+            {hisagiWinCalc(1)}
+          </div>
+        ))}
+      </details>
+    </div>
   );
 }
 
