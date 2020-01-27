e1544bb36e06f65aa03da50359f8f66494c0fd15
yotsugi <harukipdr@gmail.com>
2020-01-28 01:41:06 +0900

    fixed ditails

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 import Header_img from "./picture/utsHisagi.jpg";
 import { match_result, uta_movie_url, hisagi_movie_url } from "./data";
+import hoko_img from "./picture/hoko.png";
+import asari_img from "./picture/asari.png";
+import area_img from "./picture/area.png";
+import tower_img from "./picture/tower.png";
+import nawabari_img from "./picture/nawabari.png";
 
 function hisagiWinCalc(order) {
   var a = 0;
@@ -21,7 +26,13 @@ function hisagiWinCalc(order) {
 function Header() {
   return (
     <header className="App-header">
-      <img src={Header_img} alt="header banner" />
+      <img
+        style={{
+          width: "100%"
+        }}
+        src={Header_img}
+        alt="header banner"
+      />
     </header>
   );
 }
@@ -29,19 +40,80 @@ function Header() {
 function Body() {
   return (
     <div>
-      <details>
+      <details
+        style={{
+          border: "1px solid",
+          borderRadius: "4px"
+        }}
+      >
         <summary>
           第一周
-          <p>HISAGI WIN {hisagiWinCalc(1)}</p>
-          <p>UTA WIN {5-hisagiWinCalc(1)}</p>
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <p>楸 {hisagiWinCalc(1)} 勝</p>
+            <p>炉龍うた {5 - hisagiWinCalc(1)} 勝</p>
+          </div>
         </summary>
         {match_result.order1.map((match, i) => (
           <div key={i}>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <p
+                style={
+                  match.team1.win
+                    ? { color: "orange", fontWeight: "900" }
+                    : { color: "blue", fontWeight: "900" }
+                }
+              >
+                {match.team1.win ? "WIN" : "LOSE"}
+              </p>
+              <p
+                style={
+                  match.team2.win
+                    ? { color: "orange", fontWeight: "900" }
+                    : { color: "blue", fontWeight: "900" }
+                }
+              >
+                {match.team2.win ? "WIN" : "LOSE"}
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around"
+              }}
+            >
+              <a href={hisagi_movie_url + match.hisagi_movie}>楸 試合リンク</a>
+              <a href={uta_movie_url + match.uta_movie}>炉龍うた 試合リンク</a>
+            </div>
             <div className="ul-team">
               <ul className="li-all">
                 {match.team1.member.player.map((player, i) => (
                   <li key={i}>{player}</li>
                 ))}
+              </ul>
+              <ul
+                style={{
+                  listStyle: "none"
+                }}
+              >
+                <li>
+                  <img
+                    src={
+                      match.rule === "ナワバリバトル"
+                        ? nawabari_img
+                        : match.rule === "ガチエリア"
+                        ? area_img
+                        : match.rule === "ガチホコバトル"
+                        ? hoko_img
+                        : match.rule === "ガチヤグラ"
+                        ? tower_img
+                        : match.rule === "ガチアサリ"
+                        ? asari_img
+                        : nawabari_img
+                    }
+                    alt="rules"
+                  />
+                </li>
+                <li>{match.rule}</li>
               </ul>
               <ul className="li-all">
                 {match.team2.member.player.map((player, i) => (
@@ -49,9 +121,6 @@ function Body() {
                 ))}
               </ul>
             </div>
-            {hisagi_movie_url}
-            {uta_movie_url}
-            {hisagiWinCalc(1)}
           </div>
         ))}
       </details>
