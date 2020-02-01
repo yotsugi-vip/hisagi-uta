8c87b424d7c029d770fcff939b45f61d6c6ee416
yotsugi <harukipdr@gmail.com>
2020-02-01 18:59:47 +0900

    header resize

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Header() {
     <header className="App-header">
       <img
         style={{
-          width: "70%"
+          width: "100%"
         }}
         src={Header_img}
         alt="header banner"
@@ -43,7 +43,8 @@ function Body() {
       <details
         style={{
           border: "1px solid",
-          borderRadius: "4px"
+          borderRadius: "4px",
+          margin:"10px 0"
         }}
         open
       >
@@ -84,8 +85,122 @@ function Body() {
               backgroundPosition: "center",
               backgroundColor: "rgba(255,255,255,0.8)",
               backgroundBlendMode: "lighten",
-              padding:"40px 0px",
-              margin:"15px 0px"
+              padding: "40px 0px",
+              margin: "15px 0px"
+            }}
+          >
+            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+              <p
+                style={
+                  match.team1.win
+                    ? {
+                        color: "orange",
+                        fontWeight: "900",
+                        fontSize: "20px",
+                        margin: "0px"
+                      }
+                    : {
+                        color: "blue",
+                        fontWeight: "900",
+                        fontSize: "20px",
+                        margin: "0px"
+                      }
+                }
+              >
+                {match.team1.win ? "WIN" : "LOSE"}
+              </p>
+              <p
+                style={
+                  match.team2.win
+                    ? {
+                        color: "orange",
+                        fontWeight: "900",
+                        fontSize: "20px",
+                        margin: "0px"
+                      }
+                    : {
+                        color: "blue",
+                        fontWeight: "900",
+                        fontSize: "20px",
+                        margin: "0px"
+                      }
+                }
+              >
+                {match.team2.win ? "WIN" : "LOSE"}
+              </p>
+            </div>
+            <div className="ul-team">
+              <ul className="li-all">
+                {match.team1.member.player.map((player, i) => (
+                  <li key={i}>{player}</li>
+                ))}
+              </ul>
+              <ul className="li-all">
+                {match.team2.member.player.map((player, i) => (
+                  <li key={i}>{player}</li>
+                ))}
+              </ul>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-evenly"
+              }}
+            >
+              <a href={hisagi_movie_url + match.hisagi_movie}>楸 試合リンク</a>
+              <a href={uta_movie_url + match.uta_movie}>炉龍うた 試合リンク</a>
+            </div>
+          </div>
+        ))}
+      </details>
+
+      <details
+        style={{
+          border: "1px solid",
+          borderRadius: "4px",
+          margin:"10px 0"
+        }}
+        open
+      >
+        <summary>
+          第二周
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              fontWeight: "900",
+              fontsize: "30px"
+            }}
+          >
+            <p>炉龍うた {5 - hisagiWinCalc(2)} 勝</p>
+            <p>楸 {hisagiWinCalc(2)} 勝</p>
+          </div>
+        </summary>
+
+        {match_result.order2.map((match, i) => (
+          <div
+            className={"match" + i}
+            key={i}
+            style={{
+              backgroundImage: `url(${
+                match.rule === "ナワバリバトル"
+                  ? nawabari_img
+                  : match.rule === "ガチエリア"
+                  ? area_img
+                  : match.rule === "ガチホコバトル"
+                  ? hoko_img
+                  : match.rule === "ガチヤグラ"
+                  ? tower_img
+                  : match.rule === "ガチアサリ"
+                  ? asari_img
+                  : nawabari_img
+              })`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundColor: "rgba(255,255,255,0.8)",
+              backgroundBlendMode: "lighten",
+              padding: "40px 0px",
+              margin: "15px 0px"
             }}
           >
             <div style={{ display: "flex", justifyContent: "space-evenly" }}>
